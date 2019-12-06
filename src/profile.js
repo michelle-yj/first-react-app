2f502f8ca995b227840d0474b605f1e36760a97c
michelleyj <michelleduhaime91@gmail.com>
2019-12-06 10:28:47 -0500

    adding profile.js

---
+++ src/profile.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Profile = () => (
+
+<div>
+  <img src="https://avatars3.githubusercontent.com/u/55298846?s=460&v=4" alt="profile pic"/>
+  <h1>Michelle Duhaime</h1>
+</div>
+);
+
+export default Profile;
