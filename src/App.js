3999016d0a167b285f5b8db317f716e8eb933e78
michelleyj <michelleduhaime91@gmail.com>
2019-12-18 20:41:49 -0500

    added new log in button, updated header components, added sample buttons

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import Profile from './profile/profile';
 import data from './data.json';
+import Button from '././components/button';
+import '././components/button.css';
+import Header from '././components/header';
+import '././components/header.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="nav-buttons">
+        <Button link="#" color="lightyellow" size="small">Home</Button>
+        <Button link="#" color="lightcoral" size="small">About</Button>
+        <Button link="#" color="tan" size="small">Services</Button>
+      </div>
         <img src={logo} className="App-logo" alt="logo" />
+          {
+            <Header className="header">My First React App!</Header>
+          }
         <div className="profiles">
           {
             data.map((person) => (
               <Profile person={person} key={person.userName} />
             ))
           }
+        </div>
+        <div className="button">
+        {
+            <Button link="#" size="large">Log In</Button>
+        }
         </div>
       </header>
     </div>
